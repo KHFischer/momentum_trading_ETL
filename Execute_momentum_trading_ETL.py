9e33954e3579cc16949e225f458023c70554a900
Kalle Fischer <111733603+KHFischer@users.noreply.github.com>
2022-12-03 14:43:24 +0100

    Create Execute_momentum_trading_ETL.py
    Run the pipeline and save outputs to local directory

---
+++ Execute_momentum_trading_ETL.py
@@ -0,0 +1,8 @@
+from ipynb.fs.defs.screen_universe import *
+from ipynb.fs.defs.sma_list import *
+from ipynb.fs.defs.rolling_method import *
+
+def main():
+    execute_screen_universe()
+    execute_sma_list()
+    execute_rolling_method()
