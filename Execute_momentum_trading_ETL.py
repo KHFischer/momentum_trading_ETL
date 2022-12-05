14f3b5debfa21a818fc5b161c17c6d7112e185b3
Kalle Fischer <111733603+KHFischer@users.noreply.github.com>
2022-12-05 15:41:51 +0100

    Update Execute_momentum_trading_ETL.py
    Fixed broken import statements

--- Execute_momentum_trading_ETL.py
+++ Execute_momentum_trading_ETL.py
@@ -1,6 +1,6 @@
-from ipynb.fs.defs.screen_universe import *
-from ipynb.fs.defs.sma_list import *
-from ipynb.fs.defs.rolling_method import *
+import screen_universe
+import sma_list
+import rolling_method
 
 def main():
     execute_screen_universe()
